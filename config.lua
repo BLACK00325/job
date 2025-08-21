c9773ab7d539d20d4d51e6b12d4e8d1ee5f9bb3c
PSYCHO_DARK <62090887+BLACK00325@users.noreply.github.com>
2025-08-21 14:32:49 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.QBExport = 'qb-core'
+
+Config.UseAce = true
+Config.QBPerms = { 'god', 'admin' }
+
+Config.OpenCommand = 'jobc'
+Config.KeyMapping = { enabled = true, key = 'F6' }
+
+Config.ActionCooldownMs = 1000
